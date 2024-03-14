d9f88c3187e0eca013f7d893e78c511cd46bddc5
Daniel <dani3lkamanthi@gmail.com>
2024-03-15 00:13:52 +0300

    Modifies files

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names, birthday) {
+    let cards = []
+    for (let i = 0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${birthday} gift!`);
+    }
+
+    return cards;
+}
+
+function countDown(firstNumber) {
+   while ( firstNumber > 0 ) {
+console.log(firstNumber);
+firstNumber -= 1;
+   }
+   console.log(firstNumber);
+}
